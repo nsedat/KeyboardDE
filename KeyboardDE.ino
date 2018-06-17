8b85130a7276b58afee11be70ed7755e342c63b9
nsedat <niels.sedat@nobot.de>
2018-06-17 17:25:35 +0200

    initial upload of local work so far
    tested and should be treated as v1.0

---
+++ KeyboardDE.ino
@@ -0,0 +1,28 @@
+/*
+ * KeyboardDE Tester 
+ */
+
+#include "KeyboardDE.h"
+
+
+void setup(){
+  for (int i=5; i>=0; i--) {  // time to click into some opened empty editor
+    Serial.print(i);
+    delay(1000);
+  }
+  
+  Keyboard.print("^1234567890ß´\n");
+  Keyboard.print("°!\"§$%&/()=?`\n");
+  Keyboard.print("qwertzuiopü+\n");
+  Keyboard.print("QWERTZUIOPÜ*\n");
+  Keyboard.print("asdfghjklöä#\n");
+  Keyboard.print("ASDFGHJKLÖÄ'\n");
+  Keyboard.print("<yxcvbnm,.-\n");
+  Keyboard.print(">YXCVBNM;:_\n");
+  Keyboard.print("²³{[]}\\@€~|µ\n");
+}
+
+void loop(){
+  delay(500);
+}
+
