946d2b4ca9bc19793b8b112bc115bfd9c5310653
nsedat <niels.sedat@nobot.de>
2018-06-21 12:01:58 +0200

    fixed issue with OTP programmer
    by adding press and release of original implementation and renaming own to *_iso

--- KeyboardDE.cpp
+++ KeyboardDE.cpp
@@ -4,9 +4,10 @@
   Copyright (c) 2015, Arduino LLC
   Original code (pre-library): Copyright (c) 2011, Peter Barrett
   
-  This version is written by Niels Sedat (2018-06-17)
+  This version is written by Niels Sedat (2018-06-21)
   ...handle all the available keys on a german keyboard
   eg. üöäÜÖÄß€ ... but also mapping all the special chars as well
+  added compatibility for On The Go Porgrammer  /
   
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
@@ -383,7 +384,7 @@ uint8_t USBPutChar(uint8_t c);
 // to the persistent key report and sends the report.  Because of the way 
 // USB HID works, the host acts like the key remains pressed until we 
 // call release(), releaseAll(), or otherwise clear the report and resend.
-size_t Keyboard_::press(uint8_t k) 
+size_t Keyboard_::press_iso(uint8_t k) 
 {
 	uint8_t i;
 
@@ -393,8 +394,8 @@ size_t Keyboard_::press(uint8_t k)
 		return 0;
 	}
 	if (k & 0x80) {						// it's a capital letter or other character reached with shift
-		_keyReport.modifiers |= 0x02;	// the left shift modifier
-		k &= 0x7F;
+    _keyReport.modifiers |= 0x02; // the left shift modifier
+    k &= 0x7F;
 	}
 	if (k & 0x40) {
 		_keyReport.modifiers |= 0x40;
@@ -428,7 +429,7 @@ size_t Keyboard_::press(uint8_t k)
 // release() takes the specified key out of the persistent key report and
 // sends the report.  This tells the OS the key is no longer pressed and that
 // it shouldn't be repeated any more.
-size_t Keyboard_::release(uint8_t k) 
+size_t Keyboard_::release_iso(uint8_t k) 
 {
 	uint8_t i;
 	k = pgm_read_byte(_asciimap_iso + k);
@@ -460,6 +461,86 @@ size_t Keyboard_::release(uint8_t k)
 }
 
 
+// press() adds the specified key (printing, non-printing, or modifier)
+// to the persistent key report and sends the report.  Because of the way 
+// USB HID works, the host acts like the key remains pressed until we 
+// call release(), releaseAll(), or otherwise clear the report and resend.
+size_t Keyboard_::press(uint8_t k) 
+{
+  uint8_t i;
+  if (k >= 136) {     // it's a non-printing key (not a modifier)
+    k = k - 136;
+  } else if (k >= 128) {  // it's a modifier key
+    _keyReport.modifiers |= (1<<(k-128));
+    k = 0;
+  } else {        // it's a printing key
+    k = pgm_read_byte(_asciimap_iso + k);
+    if (!k) {
+      setWriteError();
+      return 0;
+    }
+    if (k & 0x80) {           // it's a capital letter or other character reached with shift
+      _keyReport.modifiers |= 0x02; // the left shift modifier
+      k &= 0x7F;
+    }
+  }
+  
+  // Add k to the key report only if it's not already present
+  // and if there is an empty slot.
+  if (_keyReport.keys[0] != k && _keyReport.keys[1] != k && 
+    _keyReport.keys[2] != k && _keyReport.keys[3] != k &&
+    _keyReport.keys[4] != k && _keyReport.keys[5] != k) {
+    
+    for (i=0; i<6; i++) {
+      if (_keyReport.keys[i] == 0x00) {
+        _keyReport.keys[i] = k;
+        break;
+      }
+    }
+    if (i == 6) {
+      setWriteError();
+      return 0;
+    } 
+  }
+  sendReport(&_keyReport);
+  return 1;
+}
+
+// release() takes the specified key out of the persistent key report and
+// sends the report.  This tells the OS the key is no longer pressed and that
+// it shouldn't be repeated any more.
+size_t Keyboard_::release(uint8_t k) 
+{
+  uint8_t i;
+  if (k >= 136) {     // it's a non-printing key (not a modifier)
+    k = k - 136;
+  } else if (k >= 128) {  // it's a modifier key
+    _keyReport.modifiers &= ~(1<<(k-128));
+    k = 0;
+  } else {        // it's a printing key
+    k = pgm_read_byte(_asciimap_iso + k);
+    if (!k) {
+      return 0;
+    }
+    if (k & 0x80) {             // it's a capital letter or other character reached with shift
+      _keyReport.modifiers &= ~(0x02);  // the left shift modifier
+      k &= 0x7F;
+    }
+  }
+  
+  // Test the key report to see if k is present.  Clear it if it exists.
+  // Check all positions in case the key is present more than once (which it shouldn't be)
+  for (i=0; i<6; i++) {
+    if (0 != k && _keyReport.keys[i] == k) {
+      _keyReport.keys[i] = 0x00;
+    }
+  }
+
+  sendReport(&_keyReport);
+  return 1;
+}
+
+
 void Keyboard_::releaseAll(void)
 {
   _keyReport.keys[0] = 0;
@@ -488,8 +569,8 @@ size_t Keyboard_::write(uint8_t c)
 		return 1;
 	}
 	
-	uint8_t p = press(c);  // Keydown
-	release(c);            // Keyup
+	uint8_t p = press_iso(c);  // Keydown
+	release_iso(c);            // Keyup
 
 	return p;              // just return the result of press() since release() almost always returns 1
 }
